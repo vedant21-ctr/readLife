4d00b6e0bbf1b7eb552c417c9ed2ed9617e86467
Vedant Satbhai <vedantsatbhai@Vedants-MacBook-Pro.local>
2026-02-17 10:06:49 +0530

    Fix: Allow frontend to connect (CORS)

--- readstream-backend/server.js
+++ readstream-backend/server.js
@@ -38,19 +38,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      process.env.FRONTEND_URL
-    ].filter(Boolean);
-
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true, // Allow any origin
   credentials: true
 }));
 app.use(morgan('dev'));
